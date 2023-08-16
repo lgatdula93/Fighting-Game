8d7e36d389f636e81ff4ab8934a5b09f1dd4eff4
Lourdes Gatdula <lgatdula93@gmail.com>
2023-08-16 10:49:20 -0700

    Update Main

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
+
+# ///////////////////////////////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////////////////////
+# /////////////////////// B T M 2 6 0 - F I N A L ///////////////////////////////////
+# ///////////////////////////////////////////////////////////////////////////////////
+# //////////////// K A L E B - S V I T L A N A - L O U R D E S //////////////////////
+# ///////////////////////////////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 import pygame, sys, threading
 
 from button import Button
 from pygame import mixer
 from fighter import Fighter
 
+# Initialize pygame
 pygame.init()
 
-
+# Set screen size and menu display
 SCREEN = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Menu")
-
 BG = pygame.image.load("assets/images/background/menu_bg.jpeg")
+
 
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/buttons/font.ttf", size)
 
-
+#######################################
+########## Function for PLAY ##########
+#######################################
 def play():
     while True:       
 
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
         
+        # Menu screen disappears and moves onto game
         SCREEN.fill("black")
         
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -38,30 +53,29 @@ def play():
         mixer.init()
         pygame.init()
 
-        #create game window
+        # Create the game/screen window ---- should be identical with menu display on line 23
         SCREEN_WIDTH = 1000
         SCREEN_HEIGHT = 600
-
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Brawler")
 
-        #set framerate
+        # Set the framerate
         clock = pygame.time.Clock()
         FPS = 60
 
-        #define colours
+        # Define colors used for Health Bar and Fonts
         RED = (255, 0, 0)
         ORANGE = (255, 165, 0)
         WHITE = (255, 255, 255)
 
-        #define game variables
+        # Define game variables
         intro_count = 3
         last_count_update = pygame.time.get_ticks()
         score = [0, 0]#player scores. [P1, P2]
         round_over = False
         ROUND_OVER_COOLDOWN = 2000
 
-        #define fighter variables
+        # Define fighter variables
         WARRIOR_SIZE = 162
         WARRIOR_SCALE = 5
         WARRIOR_OFFSET = [72, 56]
@@ -71,7 +85,7 @@ def play():
         WIZARD_OFFSET = [112, 107]
         WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
 
-        #load music and sounds
+        # Load music and sounds
         pygame.mixer.music.load("assets/audio/alexander-nakarada-chase.mp3")
         pygame.mixer.music.set_volume(0.5)
         pygame.mixer.music.play(-1, 0.0, 5000)
@@ -80,82 +94,81 @@ def play():
         magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
         magic_fx.set_volume(0.75)
 
-        #load background image
+        # Load background image
         bg_image = pygame.image.load("assets/images/background/level-1.gif").convert_alpha()
 
-        #load spritesheets
+        # Load spritesheets
         warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
         wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").convert_alpha()
 
-        #load vicory image
+        # Load victory image
         victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha()
 
-        #define number of steps in each animation
+        # Define number of steps in each animation
         WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
         WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 
-        #define font
+        # Define fonts
         count_font = pygame.font.Font("assets/fonts/Hare-.otf", 80)
         score_font = pygame.font.Font("assets/fonts/Hare-.otf", 30)
 
-        #function for drawing text
+        # Function for drawing text
         def draw_text(text, font, text_col, x, y):
           img = font.render(text, True, text_col)
           screen.blit(img, (x, y))
 
-        #function for drawing background
+        # Function for drawing background
         def draw_bg():
           scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
           screen.blit(scaled_bg, (0, 0))
 
-        #function for drawing fighter health bars
+        # Function for drawing fighter health bars
         def draw_health_bar(health, x, y):
           ratio = health / 100
           pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
           pygame.draw.rect(screen, RED, (x, y, 400, 30))
           pygame.draw.rect(screen, ORANGE, (x, y, 400 * ratio, 30))
 
-
-        #create two instances of fighters
+        # Create two instances of fighters
         fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
         fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
 
-        #game loop
+        # Game Loop
         run = True
         while run:
           clock.tick(FPS)
 
-          #draw background
+          # Draw background
           draw_bg()
 
-          #show player stats
+          # Show player stats
           draw_health_bar(fighter_1.health, 20, 20)
           draw_health_bar(fighter_2.health, 580, 20)
           draw_text("WARRIOR: " + str(score[0]), score_font, RED, 20, 60)
           draw_text("WIZARD: " + str(score[1]), score_font, RED, 580, 60)
 
-          #update countdown
+          # Update countdown
           if intro_count <= 0:
-            #move fighters
+            # Move fighters
             fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
             fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
           else:
-            #display count timer
+            # Display count timer
             draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
-            #update count timer
+            # Update count timer
             if (pygame.time.get_ticks() - last_count_update) >= 1000:
               intro_count -= 1
               last_count_update = pygame.time.get_ticks()
 
-          #update fighters
+          # Update fighters
           fighter_1.update()
           fighter_2.update()
 
-          #draw fighters
+          # Draw fighters
           fighter_1.draw(screen)
           fighter_2.draw(screen)
 
-          #check for player defeat
+          # Check for player defeat
           if round_over == False:
             if fighter_1.alive == False:
               score[1] += 1
@@ -166,7 +179,7 @@ def play():
               round_over = True
               round_over_time = pygame.time.get_ticks()
           else:
-            #display victory image
+            # Display VICTORY image
             screen.blit(victory_img, (300, 250))
             if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
               round_over = False
@@ -174,18 +187,23 @@ def play():
               fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
               fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
 
-          #event handler
+          # Event handler
           for event in pygame.event.get():
             if event.type == pygame.QUIT:
               run = False
 
 
-          #update display
+          # Update display
           pygame.display.update()
 
-        #exit pygame
+        # Exit pygame
         pygame.quit()
-    
+
+
+#######################################
+######## Function for CONTROL #########
+#######################################
+
 def control():
     while True:
         CONTROL_MOUSE_POS = pygame.mouse.get_pos()
@@ -211,6 +229,11 @@ def control():
                     main_menu()
 
         pygame.display.update()
+
+
+#######################################
+######### M A I N - M E N U  ##########
+#######################################
 
 def main_menu():
     while True:
